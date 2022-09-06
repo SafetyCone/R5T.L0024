162c5f00483e0eb47135d4da3f4163a01ea73c16
davidcoats <david@getsafetycone.com>
2022-09-05 22:21:41 -0700

    Interim changes.

---
+++ source/R5T.L0024/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.L0024
+{
+	/// <summary>
+	/// An SSH.NET NuGet package selector.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
---
+++ source/R5T.L0024/Code/Instances.cs
@@ -0,0 +1,10 @@
+using System;
+
+
+namespace R5T.L0024
+{
+    public static class Instances
+    {
+		
+    }
+}
